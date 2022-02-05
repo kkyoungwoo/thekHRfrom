390fb3827d84070678eabf7bf0a30b3c0859df50
coder <bizdb@kakao.com>
2022-02-05 10:59:25 +0900

    setting_start 2022.2.5
    Cheer up bae

---
+++ src/components/a_homepage/App.jsx
@@ -0,0 +1,14 @@
+import Header from '../b_header/Header'
+import Footer from '../c_footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      세팅시작
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
