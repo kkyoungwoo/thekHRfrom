154af72fb08eb431a9f1c9f78c67f0b4ff491250
coder <bizdb@kakao.com>
2023-08-22 11:07:05 +0900

    8.22

--- src/components/homepage/footer/Footer.jsx
+++ src/components/homepage/footer/Footer.jsx
@@ -3,8 +3,14 @@ import './footer.css'
 
 function Footer() {
     return (
-        <div className='footer' style={{height: "0px"}}>
+        <div className='footer'>
             <div className='pc_footer'>
+                <ul>
+                    <li>워크비자</li>
+                    <li>E-MAIL : workvisa@naver.com</li>
+                    <li>사업자등록번호 : 884-18-01907</li>
+                    <li>직업제공사업 신고번호 : J1400020230002</li>
+                </ul>
             </div>
         </div>
     )
