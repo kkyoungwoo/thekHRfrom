006efe1086b1fae0df9edabf991d6c71c22df32a
coder <bizdb@kakao.com>
2023-08-23 14:20:53 +0900

    8.23

--- src/components/homepage/footer/Footer.jsx
+++ src/components/homepage/footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className='footer'>
             <div className='pc_footer'>
                 <ul>
-                    <li>워크비자</li>
+                    <li>워크비자<span style={{marginLeft:"20px"}}>고경우</span></li>
                     <li>E-MAIL : workvisa@naver.com</li>
                     <li>사업자등록번호 : 884-18-01907</li>
                     <li>직업제공사업 신고번호 : J1400020230002</li>
